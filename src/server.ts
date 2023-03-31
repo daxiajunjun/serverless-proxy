00710219e962a39c02f2141e11f176e436708521
daxiajunjun <wangjunjun6@126.com>
2023-04-01 02:36:18 +0800

    feat:proxy

---
+++ src/server.ts
@@ -0,0 +1,55 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as serverless from 'serverless-http';
+import * as helmet from 'helmet';
+import { AppModule } from './module';
+import * as proxy from 'express-http-proxy';
+
+function proxyOpenAIHost() {
+  return 'https://api.openai.com';
+}
+
+const bootstrap = async (module: any) => {
+  const app = express();
+  const nestApp = await NestFactory.create(module, new ExpressAdapter(app));
+
+  nestApp.setGlobalPrefix('/.netlify/functions/server');
+  nestApp.enableCors();
+  nestApp.use(helmet());
+  nestApp.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  nestApp.use(
+    '/.netlify/functions/server/proxy_chat',
+    proxy(proxyOpenAIHost, {
+      proxyReqPathResolver: function (req) {
+        return '/v1/chat/completions';
+      },
+    }),
+  );
+  nestApp.use(express.json({ limit: '50mb' }));
+  nestApp.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+  await nestApp.init();
+  // nestApp.listen(3005);
+  return app;
+};
+
+// bootstrap(AppModule);
+
+let cachedHadler: any;
+const proxyApi = async (module: any, event: any, context: any) => {
+  if (!cachedHadler) {
+    const app = await bootstrap(module);
+    cachedHadler = serverless(app);
+  }
+
+  return cachedHadler(event, context);
+};
+
+export const handler = async (event: any, context: any) =>
+  proxyApi(AppModule, event, context);
