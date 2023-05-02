66c5ec513d76b51eb5db4d8b1b89c8e19f259840
daxiajunjun <wangjunjun6@126.com>
2023-05-02 22:16:59 +0800

    vercel部署

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,28 @@
-const proxy = require('express-http-proxy');
-const app = require('express')();
-const cors = require('cors');
-const helmet = require('helmet');
+// const proxy = require('express-http-proxy');
+// const app = require('express')();
+// const cors = require('cors');
+// const helmet = require('helmet');
 
-function proxyOpenAIHost() {
-  return 'https://api.openai.com';
+// function proxyOpenAIHost() {
+//   return 'https://api.openai.com';
+// }
+// app.use(helmet());
+// app.use(cors());
+// app.use(
+//   '/proxy_openai/chat/completions',
+//   proxy(proxyOpenAIHost, {
+//     proxyReqPathResolver: function () {
+//       return '/v1/chat/completions';
+//     },
+//   }),
+// );
+
+// app.use('/', (req, res) => {
+//   res.send('hello world');
+// });
+
+// app.listen(3005);
+// console.log('server open');
+export default async function handler(req, res) {
+  res.status(200).send(`Hello world!`);
 }
-app.use(helmet());
-app.use(cors());
-app.use(
-  '/proxy_openai/chat/completions',
-  proxy(proxyOpenAIHost, {
-    proxyReqPathResolver: function () {
-      return '/v1/chat/completions';
-    },
-  }),
-);
-
-app.use('/', (req, res) => {
-  res.send('hello world');
-});
-
-app.listen(3005);
-console.log('server open');
