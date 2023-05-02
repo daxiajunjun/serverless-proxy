a7a55242baa69ab925461d2d60311dd2a0afd7c5
daxiajunjun <wangjunjun6@126.com>
2023-05-02 19:06:18 +0800

    feat: vercel部署

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,47 @@
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import * as express from 'express';
+// import { AppModule } from './module';
+// import * as proxy from 'express-http-proxy';
+
+// function proxyOpenAIHost() {
+//   return 'https://api.openai.com';
+// }
+
+// const bootstrap = async (module: any) => {
+//   const app = express();
+//   const nestApp = await NestFactory.create(module, new ExpressAdapter(app));
+
+//   nestApp.enableCors();
+//   nestApp.useGlobalPipes(
+//     new ValidationPipe({
+//       transform: true,
+//     }),
+//   );
+//   nestApp.use(
+//     '/proxy_openai',
+//     proxy(proxyOpenAIHost, {
+//       proxyReqPathResolver: function () {
+//         return '/v1/chat/completions';
+//       },
+//     }),
+//   );
+//   nestApp.use(express.json({ limit: '50mb' }));
+//   nestApp.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+//   await nestApp.init();
+//   nestApp.listen(3005);
+//   return app;
+// };
+
+// bootstrap(AppModule);
+
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 import { AppModule } from './module';
-import * as proxy from 'express-http-proxy';
 
-function proxyOpenAIHost() {
-  return 'https://api.openai.com';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-
-const bootstrap = async (module: any) => {
-  const app = express();
-  const nestApp = await NestFactory.create(module, new ExpressAdapter(app));
-
-  nestApp.enableCors();
-  nestApp.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
-  nestApp.use(
-    '/proxy_openai',
-    proxy(proxyOpenAIHost, {
-      proxyReqPathResolver: function () {
-        return '/v1/chat/completions';
-      },
-    }),
-  );
-  nestApp.use(express.json({ limit: '50mb' }));
-  nestApp.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-  await nestApp.init();
-  nestApp.listen(3005);
-  return app;
-};
-
-bootstrap(AppModule);
+bootstrap();
