5392040cee78d11a523a8e515e1c6e4521fb0005
daxiajunjun <wangjunjun6@126.com>
2023-05-02 19:12:36 +0800

    feat: vercal 部署

--- src/domains/randoms/constants/randoms.constant.ts
+++
@@ -1,2 +0,0 @@
-export const SUCCESS_GENERATE_RANDOM = 'success generate random';
-export const SUCCESS_HASHED_STRING = 'success hashed string';
--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller';
-import { RandomsModule } from './domains/randoms/randoms.module';
 import { AppService } from './service';
 
 @Module({
-  imports: [RandomsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
