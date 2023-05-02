fff5f628c5f9a3d9eaa44ef24b8efed7842e0b88
daxiajunjun <wangjunjun6@126.com>
2023-05-02 19:34:47 +0800

    feat: nestjs deploy

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,25 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import { AppModule } from './module';
-import * as proxy from 'express-http-proxy';
-
-function proxyOpenAIHost() {
-  return 'https://api.openai.com';
-}
-
-const bootstrap = async (module: any) => {
-  const app = express();
-  const nestApp = await NestFactory.create(module, new ExpressAdapter(app));
-
-  nestApp.enableCors();
-  nestApp.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
-  nestApp.use(
-    '/proxy_openai',
-    proxy(proxyOpenAIHost, {
-      proxyReqPathResolver: function () {
-        return '/v1/chat/completions';
-      },
-    }),
-  );
-  nestApp.use(express.json({ limit: '50mb' }));
-  nestApp.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-  await nestApp.init();
-  nestApp.listen(3005);
-  return app;
-};
-
-bootstrap(AppModule);
-
+// import { ValidationPipe } from '@nestjs/common';
 // import { NestFactory } from '@nestjs/core';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import * as express from 'express';
 // import { AppModule } from './module';
 // import * as proxy from 'express-http-proxy';
+
 // function proxyOpenAIHost() {
 //   return 'https://api.openai.com';
 // }
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors();
-//   app.use(
+// const bootstrap = async (module: any) => {
+//   const app = express();
+//   const nestApp = await NestFactory.create(module, new ExpressAdapter(app));
+
+//   nestApp.enableCors();
+//   nestApp.useGlobalPipes(
+//     new ValidationPipe({
+//       transform: true,
+//     }),
+//   );
+//   nestApp.use(
 //     '/proxy_openai',
 //     proxy(proxyOpenAIHost, {
 //       proxyReqPathResolver: function () {
@@ -55,6 +27,34 @@ bootstrap(AppModule);
 //       },
 //     }),
 //   );
-//   await app.listen(3000);
-// }
-// bootstrap();
+//   nestApp.use(express.json({ limit: '50mb' }));
+//   nestApp.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+//   await nestApp.init();
+//   nestApp.listen(3005);
+//   return app;
+// };
+
+// bootstrap(AppModule);
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './module';
+import * as proxy from 'express-http-proxy';
+function proxyOpenAIHost() {
+  return 'https://api.openai.com';
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(
+    'proxy_openai',
+    proxy(proxyOpenAIHost, {
+      proxyReqPathResolver: function () {
+        return '/v1/chat/completions';
+      },
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
